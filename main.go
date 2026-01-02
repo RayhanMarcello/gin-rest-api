382e46a30c35b57a1fa62719eac3887c8068a092
Rayhan Marcello <rayhanmarcello222@gmail.com>
2026-01-03 01:56:37 +0800

    create auto migrate db

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"belajar-golang/handler"
+	"belajar-golang/product"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
 	router := gin.Default()
+
+	dsn := "app:root@tcp(127.0.0.1:3306)/belajar_golang?charset=utf8mb4&parseTime=True&loc=Local"
+  	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil{
+		fmt.Println("connection db err")
+	}
+
+	fmt.Println("db connection sucsess")
+
+	db.AutoMigrate(&product.Product{})
+
 	v1 := router.Group("/v1")
 
 	v1.GET("/", handler.RootHandler)
